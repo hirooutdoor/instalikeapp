5e4025568002e8514c56427ac793802ab2008d10
hiro <>
2020-12-25 15:08:18 +0900

    create profile table

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,12 @@
+class ProfilesController < ApplicationController
+
+    def show
+        @user
+    end
+
+    def edit
+    end
+
+    def update
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
+
+  resource :profile, only: [:show, :edit, :update]
 end
