6923ce548b397b5d20df85cd242992489fe24e55
hiro <>
2020-12-29 16:21:55 +0900

    add user_id column

--- db/schema.rb
+++ db/schema.rb
@@ -46,10 +46,12 @@ ActiveRecord::Schema.define(version: 2020_12_28_070526) do
 
   create_table "comments", force: :cascade do |t|
     t.bigint "article_id", null: false
+    t.bigint "user_id", null: false
     t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
